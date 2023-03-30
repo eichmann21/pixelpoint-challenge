d76f21dcec84eef816f30b90073c9aa27733878c
Nico <eichmann.nico@gmail.com>
2023-03-30 20:18:34 +0200

    dynamic page

---
+++ pages/event/_id.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Event Nummer: {{this.$route.params.id}}</h1>
+    </div>
+</template>
+
+<script setup>
+const test=
+console.log()
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  <p>Erster Test mit Nuxt Framework</p>
+  <NuxtLink to="/event/1">About Page</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+</script>
